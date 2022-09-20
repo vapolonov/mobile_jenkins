ff1866602e129f3b57e4ec58bd2b50f86233e2dd
vasvap <vasvap@gmail.com>
2022-09-20 23:38:42 +0300

    fix

--- tests/browserstack_sample.py
+++ tests/browserstack_sample.py
@@ -1,23 +1,62 @@
-
-from allure import step
-from appium.webdriver.common.appiumby import AppiumBy
-from selene import have
-
-from selene.support.shared import browser
+"""
+Test wiki app
+"""
+import allure
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from tests.conftest import create_driver
 from utils.attach import add_video
 
 
+@allure.tag('mobile')
+@allure.title('Test search')
 def test_search():
+    """
+    Testing search form
+    """
     driver = create_driver(test_search)
-    with step('Type search request'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).send_keys("BrowserStack")
 
-    with step('Verify content found'):
-        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
+    search_element = WebDriverWait(driver, 30).until(
+        EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "Search Wikipedia"))
+    )
+    search_element.click()
+    search_input = WebDriverWait(driver, 30).until(
+        EC.element_to_be_clickable((MobileBy.ID, "org.wikipedia.alpha:id/search_src_text"))
+    )
+    search_input.send_keys("BrowserStack")
+    search_results = driver.find_elements_by_class_name("android.widget.TextView")
+
+    assert len(search_results) > 0, 'List should be more 0'
 
     add_video(driver.session_id, 'Search Wikipedia')
+
+    driver.quit()
+
+
+@allure.tag('mobile')
+@allure.title('Test hide news')
+def test_hide_news():
+    """
+    Testing hide news
+    """
+    driver = create_driver(test_hide_news)
+
+    first_block_news = driver.find_elements_by_id("org.wikipedia.alpha:id/view_card_header_title")[0]
+    first_header_menu = driver.find_elements_by_id("org.wikipedia.alpha:id/view_list_card_header_menu")[0]
+    title_first_block = first_block_news.text
+
+    first_header_menu.click()
+    item_menu = WebDriverWait(driver, 30).until(
+        EC.element_to_be_clickable((MobileBy.ID, "org.wikipedia.alpha:id/title")))
+    item_menu.click()
+
+    new_first_block_news = driver.find_elements_by_id("org.wikipedia.alpha:id/view_card_header_title")[0]
+    title_new_first_block = new_first_block_news.text
+
+    assert title_first_block is not title_new_first_block, 'Titles should be different'
+
+    add_video(driver.session_id, 'Testing hide news')
 
     driver.quit()
